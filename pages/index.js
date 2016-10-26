a93ed5ff364d114fb828510a8a38069b9a51b4fa
Julia Sun <julia.sun@berkeley.edu>
2016-10-25 22:35:06 -0700

    made basic pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import Scroll from 'react-scroll';
 import { config } from 'config';
 import { Link } from 'react-router';
 import { prefixLink } from 'gatsby-helpers';
 
-import $ from 'jquery';
 import _ from 'lodash';
 
 export default class Index extends React.Component {
@@ -16,19 +16,33 @@ export default class Index extends React.Component {
 
   render () {
     return (
-      <div className="section section--home">
-        <Helmet
-          title={config.siteTitle}
-        />
-        <div className="title__wrapper">
-          <img className="cmyk--horizontal" src="/img/cmyk.png"/>
-          <img className="cmyk--square" src="/img/cmyk-square.png"/>
+      <div>
+        <div className="section section--home">
+          <Helmet
+            title={config.siteTitle}
+          />
+          <div className="title__wrapper">
+            <img className="cmyk--horizontal" src="/img/cmyk.png"/>
+            <img className="cmyk--square" src="/img/cmyk-square.png"/>
+          </div>
+          <div className="navigation">
+            <div className="nav__item" id="about">about</div>
+            <div className="nav__item" id="schedule">schedule</div>
+            <div className="nav__item" id="registration">registration</div>
+            <div className="nav__item" id="competition">competition</div>
+          </div>
         </div>
-        <div className="navigation">
-          <div className="nav__item" id="about">about</div>
-          <div className="nav__item" id="schedule">schedule</div>
-          <div className="nav__item" id="registration">registration</div>
-          <div className="nav__item" id="competition">competition</div>
+        <div className="section section--about">
+          about
+        </div>
+        <div className="section section--schedule">
+          schedule
+        </div>
+        <div className="section section--registration">
+          registration
+        </div>
+        <div className="section section--competition">
+          competition
         </div>
       </div>
     );
