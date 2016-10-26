8496cb52aafd42e1e642b81f4f0addf8c931d8a5
Christian Le <christianle94@gmail.com>
2016-10-25 22:34:58 -0700

    InteractiveTitle Component and cleanup

---
+++ components/InteractiveTitle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { prefixLink } from 'gatsby-helpers';
+
+export default class InteractiveTitle extends React.Component {
+  render () {
+    return (
+      <div className="interactive-title">
+      </div>
+    );
+  }
+}
+
--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,17 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { config } from 'config';
 import { Link } from 'react-router';
-import { prefixLink } from 'gatsby-helpers';
 
-import $ from 'jquery';
-import _ from 'lodash';
+import InteractiveTitle from '../components/InteractiveTitle';
 
 export default class Index extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {}
-
   render () {
     return (
       <div className="section section--home">
         <Helmet
           title={config.siteTitle}
         />
-        <div className="title__wrapper">
-          <img className="cmyk--horizontal" src="/img/cmyk.png"/>
-          <img className="cmyk--square" src="/img/cmyk-square.png"/>
-        </div>
+        <InteractiveTitle />
         <div className="navigation">
           <div className="nav__item" id="about">about</div>
           <div className="nav__item" id="schedule">schedule</div>
@@ -34,3 +23,4 @@ export default class Index extends React.Component {
     );
   }
 }
+
